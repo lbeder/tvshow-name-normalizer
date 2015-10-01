abdf6d7b8736820fde19fd08a8ff10ddd5aa0312
Leonid Beder <leonid.beder@gmail.com>
2015-10-01 18:25:51 +0300

    Fixed script.

--- scripts/normalize_directory.bat
+++ scripts/normalize_directory.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-bundle exec ruby ../tvshow_name_normalizer.rb normalize_directory --path=%cd% --recursive
+bundle exec ruby ../tvshow_name_normalizer.rb normalize_directory --path=%1 --recursive
 
 PAUSE
